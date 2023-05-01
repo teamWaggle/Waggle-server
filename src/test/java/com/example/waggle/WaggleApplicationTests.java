7ea15ab426a2f700622d8d99d38aa24fbafa0af4
suddiyo <suddiyo@naver.com>
2023-05-01 15:25:15 +0900

    Initial commit

---
+++ src/test/java/com/example/waggle/WaggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
