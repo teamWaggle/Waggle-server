a0284a66881741dda68236dfab1e3b792c9cf550
hann <wjdgks1233@gmail.com>
2023-05-30 15:28:55 +0900

    REF:board->findById 수정

--- src/main/java/com/example/waggle/repository/board/boardtype/QuestionRepository.java
+++ src/main/java/com/example/waggle/repository/board/boardtype/QuestionRepository.java
@@ -9,10 +9,6 @@ import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
-    //BoardId를 통해 question 하나를 조회
-    @Query(value = "select * from question join board on board.board_id = :boardId", nativeQuery = true)
-    Question findByBoardId(@Param("boardId")Long id);
-
     //hashtag를 통해 board를 모두 가져온다.(question에 한해서)
     @Query(value = "SELECT * FROM question AS q" +
             " JOIN board AS b ON q.board_id = b.board_id" +
