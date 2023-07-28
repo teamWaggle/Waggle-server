c311da38a237e2ff6f3769c54a16607f03d874c9
suddiyo <suddii01@sju.ac.kr>
2023-07-29 02:55:17 +0900

    Feat: index template 및 IndexController 구현

--- src/main/java/com/example/waggle/controller/IndexController.java
+++ src/main/java/com/example/waggle/controller/IndexController.java
@@ -1,26 +1,63 @@
 package com.example.waggle.controller;
 
 import com.example.waggle.component.file.FileStore;
+import com.example.waggle.dto.board.StorySimpleDto;
+import com.example.waggle.service.board.StoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class IndexController {
 
+    private final StoryService storyService;
     private final FileStore fileStore;
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+//        storyService.findAllStory();
+
+        List<StorySimpleDto> storySimpleDtoList = new ArrayList<>();
+        StorySimpleDto storySimpleDto1 = StorySimpleDto.builder()
+                .username("story1")
+                .thumbnail("https://github.com/suddiyo/suddiyo/assets/88311377/9323405a-58ba-4c41-adb6-3b2c93f5558a").build();
+
+        StorySimpleDto storySimpleDto2 = StorySimpleDto.builder()
+                .username("story2")
+                .thumbnail("https://github.com/suddiyo/suddiyo/assets/88311377/9323405a-58ba-4c41-adb6-3b2c93f5558a").build();
+
+        StorySimpleDto storySimpleDto3 = StorySimpleDto.builder()
+                .username("story3")
+                .thumbnail("https://github.com/suddiyo/suddiyo/assets/88311377/9323405a-58ba-4c41-adb6-3b2c93f5558a").build();
+
+        StorySimpleDto storySimpleDto4 = StorySimpleDto.builder()
+                .username("story4")
+                .thumbnail("https://github.com/suddiyo/suddiyo/assets/88311377/9323405a-58ba-4c41-adb6-3b2c93f5558a").build();
+
+        StorySimpleDto storySimpleDto5 = StorySimpleDto.builder()
+                .username("story5")
+                .thumbnail("https://github.com/suddiyo/suddiyo/assets/88311377/9323405a-58ba-4c41-adb6-3b2c93f5558a").build();
+
+
+        storySimpleDtoList.add(storySimpleDto1);
+        storySimpleDtoList.add(storySimpleDto2);
+        storySimpleDtoList.add(storySimpleDto3);
+        storySimpleDtoList.add(storySimpleDto4);
+        storySimpleDtoList.add(storySimpleDto5);
+
+        model.addAttribute("storySimpleDtoList", storySimpleDtoList);
         return "index";
     }
 
