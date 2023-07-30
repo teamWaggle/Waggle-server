a7b234d9d0d39f8250a6a2d5fee554b3af6e42b3
suddiyo <suddii01@sju.ac.kr>
2023-07-31 00:50:32 +0900

    Feat: index template 프로필 보완

--- src/main/java/com/example/waggle/controller/IndexController.java
+++ src/main/java/com/example/waggle/controller/IndexController.java
@@ -1,12 +1,15 @@
 package com.example.waggle.controller;
 
+import com.example.waggle.component.TestDataInit;
 import com.example.waggle.component.file.FileStore;
 
 import com.example.waggle.component.jwt.SecurityUtil;
 import com.example.waggle.dto.board.story.StorySimpleViewDto;
+import com.example.waggle.dto.board.story.StoryWriteDto;
 import com.example.waggle.dto.member.MemberSimpleDto;
 import com.example.waggle.service.board.StoryService;
 import com.example.waggle.service.member.MemberService;
+import com.example.waggle.utils.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.MalformedURLException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,40 +36,13 @@ public class IndexController {
 
     @GetMapping("/")
     public String home(Model model) {
-//        storyService.findAllStory();
 
-        List<StorySimpleViewDto> storySimpleViewDtos = new ArrayList<>();
-        StorySimpleViewDto storySimpleViewDto1 = StorySimpleViewDto.builder()
-                .username("story1")
-                .thumbnail("https://github.com/suddiyo/suddiyo/assets/88311377/9323405a-58ba-4c41-adb6-3b2c93f5558a").build();
-
-        StorySimpleViewDto storySimpleViewDto2 = StorySimpleViewDto.builder()
-                .username("story2")
-                .thumbnail("https://github.com/suddiyo/suddiyo/assets/88311377/9323405a-58ba-4c41-adb6-3b2c93f5558a").build();
-
-        StorySimpleViewDto storySimpleViewDto3 = StorySimpleViewDto.builder()
-                .username("story3")
-                .thumbnail("https://github.com/suddiyo/suddiyo/assets/88311377/9323405a-58ba-4c41-adb6-3b2c93f5558a").build();
-
-        StorySimpleViewDto storySimpleViewDto4 = StorySimpleViewDto.builder()
-                .username("story4")
-                .thumbnail("https://github.com/suddiyo/suddiyo/assets/88311377/9323405a-58ba-4c41-adb6-3b2c93f5558a").build();
-
-        StorySimpleViewDto storySimpleViewDto5 = StorySimpleViewDto.builder()
-                .username("story5")
-                .thumbnail("https://github.com/suddiyo/suddiyo/assets/88311377/9323405a-58ba-4c41-adb6-3b2c93f5558a").build();
-
-
-        storySimpleViewDtos.add(storySimpleViewDto1);
-        storySimpleViewDtos.add(storySimpleViewDto2);
-        storySimpleViewDtos.add(storySimpleViewDto3);
-        storySimpleViewDtos.add(storySimpleViewDto4);
-        storySimpleViewDtos.add(storySimpleViewDto5);
-
+        List<StorySimpleViewDto> storySimpleViewDtos = storyService.findAllStory();
         MemberSimpleDto memberSimpleDto = memberService.findMemberSimpleDto(SecurityUtil.getCurrentUsername());
-        model.addAttribute("memberSimpleDto", memberSimpleDto);
 
+        model.addAttribute("memberSimpleDto", memberSimpleDto);
         model.addAttribute("storySimpleViewDtos", storySimpleViewDtos);
+
         return "index";
     }
 
