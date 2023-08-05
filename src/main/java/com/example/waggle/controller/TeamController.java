129a24e14e4db7207ba6543360521b3748bfed15
suddiyo <suddii01@sju.ac.kr>
2023-08-06 02:51:49 +0900

    Feat: team 탭 및 팀원란 생성

--- src/main/java/com/example/waggle/controller/TeamController.java
+++ src/main/java/com/example/waggle/controller/TeamController.java
@@ -1,17 +1,22 @@
 package com.example.waggle.controller;
 
 import com.example.waggle.component.jwt.SecurityUtil;
+import com.example.waggle.dto.member.MemberDto;
 import com.example.waggle.dto.member.MemberSimpleDto;
+import com.example.waggle.dto.team.ScheduleDto;
 import com.example.waggle.dto.team.TeamDto;
 import com.example.waggle.service.member.MemberService;
 import com.example.waggle.service.team.TeamService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,9 +26,26 @@ public class TeamController {
     private final MemberService memberService;
     private final TeamService teamService;
 
+    @GetMapping("/{teamId}/members")
+    public ResponseEntity<List<MemberSimpleDto>> getTeamSchedules(@PathVariable Long teamId) {
+        List<MemberSimpleDto> memberSimpleDtos = new ArrayList<>();
+
+        Optional<TeamDto> byTeamId = teamService.findByTeamId(teamId);
+        if(byTeamId.isPresent()) {
+            TeamDto teamDto = byTeamId.get();
+            List<String> teamMembers = teamDto.getTeamMembers();
+
+            for (String teamMember : teamMembers) {
+                memberSimpleDtos.add(memberService.findMemberSimpleDto(teamMember));
+            }
+
+        }
+        return ResponseEntity.ok(memberSimpleDtos);
+    }
+
     @GetMapping("/create")
     public String createTeamForm(Model model) {
-        return "team/addTeam";
+        return "schedule/team/addTeam";
     }
 
     @PostMapping("/create")
