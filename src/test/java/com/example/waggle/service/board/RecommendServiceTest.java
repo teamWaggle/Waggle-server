589508d4f2a5b03c52cf33b8ed2c1bc197925d4f
hann <wjdgks1233@gmail.com>
2023-08-05 10:12:36 +0900

    FEAT: recommend service test완료

---
+++ src/test/java/com/example/waggle/service/board/RecommendServiceTest.java
@@ -0,0 +1,132 @@
+package com.example.waggle.service.board;
+
+import com.example.waggle.annotation.withMockUser.WithMockCustomUser;
+import com.example.waggle.component.DatabaseCleanUp;
+import com.example.waggle.dto.board.story.StoryViewDto;
+import com.example.waggle.dto.board.story.StoryWriteDto;
+import com.example.waggle.dto.member.SignInDto;
+import com.example.waggle.dto.member.SignUpDto;
+import com.example.waggle.service.member.MemberService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RecommendServiceTest {
+
+    @Autowired
+    private StoryService storyService;
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private RecommendService recommendService;
+    @Autowired
+    DatabaseCleanUp databaseCleanUp;
+
+
+    SignUpDto signUpDto1;
+    SignUpDto signUpDto2;
+    SignInDto signInDto;
+
+    StoryWriteDto storyWriteDto1;
+    StoryWriteDto storyWriteDto2;
+
+    List<String> tags1 = new ArrayList<>();
+    List<String> tags2 = new ArrayList<>();
+    List<String> medias1 = new ArrayList<>();
+    List<String> medias2 = new ArrayList<>();
+
+
+    @BeforeEach
+    void setDto() {
+        tags1.add("choco");
+        tags1.add("poodle");
+        tags2.add("poodle");
+
+        medias1.add("media1");
+        medias1.add("mediamedia1");
+        medias2.add("media2");
+        medias2.add("mediamedia2");
+
+        signUpDto1 = SignUpDto.builder()
+                .username("member1")
+                .password("12345678")
+                .nickname("닉네임1")
+                .address("서울시 광진구")
+                .phone("010-1234-5678")
+                .build();
+
+        signUpDto2 = SignUpDto.builder()
+                .username("member2")
+                .password("12345678")
+                .nickname("닉네임2")
+                .address("서울시 광진구")
+                .phone("010-1234-5678")
+                .build();
+
+        storyWriteDto1 = StoryWriteDto.builder()
+                .content("i love my choco")
+                .hashtags(tags1)
+                .medias(medias1)
+                .thumbnail("www.waggle")
+                .build();
+
+        storyWriteDto2 = StoryWriteDto.builder()
+                .content("how can i do make he is happy?")
+                .hashtags(tags2)
+                .medias(medias2)
+                .thumbnail("www.waggle")
+                .build();
+
+    }
+    @AfterEach
+    void clean() {
+        databaseCleanUp.truncateAllEntity();
+    }
+
+    private void setBoardAndMember() {
+        //member set
+        memberService.signUp(signUpDto1);
+        memberService.signUp(signUpDto2);
+
+        //story set
+        storyService.saveStory(storyWriteDto1);
+        storyService.saveStory(storyWriteDto2);
+    }
+
+    @Test
+    @WithMockCustomUser
+    void recommendBoard() {
+        //given
+        setBoardAndMember();
+        recommendService.clickRecommend(1L,"story");
+
+        //when
+        StoryViewDto storyViewByBoardId = storyService.findStoryViewByBoardId(1L);
+
+        //then
+        assertThat(storyViewByBoardId.getRecommendCount()).isEqualTo(1);
+    }
+    @Test
+    @WithMockCustomUser
+    void cancelRecommendBoard() {
+        //given
+        setBoardAndMember();
+        recommendService.clickRecommend(1L,"story");
+        recommendService.clickRecommend(1L,"story");
+
+        //when
+        StoryViewDto storyViewByBoardId = storyService.findStoryViewByBoardId(1L);
+
+        //then
+        assertThat(storyViewByBoardId.getRecommendCount()).isEqualTo(0);
+    }
+}
