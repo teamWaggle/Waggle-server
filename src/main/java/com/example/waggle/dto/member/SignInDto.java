4ffa3f2a4dabded3143a29eefa4d7c9b9629ec21
hann <wjdgks1233@gmail.com>
2023-08-15 08:08:51 +0900

    FEAT: Bean validation 설정

--- src/main/java/com/example/waggle/dto/member/SignInDto.java
+++ src/main/java/com/example/waggle/dto/member/SignInDto.java
@@ -1,6 +1,9 @@
 package com.example.waggle.dto.member;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
@@ -9,6 +12,9 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class SignInDto {
+    @NotBlank
     private String username;
+    @NotBlank
+    @Length(min=10)
     private String password;
 }
