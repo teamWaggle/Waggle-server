92c9495cc26850256bbca2f3a3ce3173d18dd04c
hann <wjdgks1233@gmail.com>
2023-08-30 21:28:38 +0900

    FEAT: validation 순서 인터페이스 생성

--- src/main/java/com/example/waggle/dto/member/SignUpDto.java
+++ src/main/java/com/example/waggle/dto/member/SignUpDto.java
@@ -3,6 +3,7 @@ package com.example.waggle.dto.member;
 import com.example.waggle.domain.member.Member;
 import com.example.waggle.domain.member.Pet;
 import com.example.waggle.domain.member.UploadFile;
+import com.example.waggle.dto.validation.ValidationGroups;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -23,17 +24,15 @@ import java.util.stream.Collectors;
 @ToString
 public class SignUpDto {
 
-    @NotBlank(message = "아이디를 작성해주세요.")
-    @Length(min = 2, max = 10, message = "아이디 길이는 최소 2자에서 최대 10자입니다.")
+    @NotBlank(message = "아이디를 작성해주세요.", groups = ValidationGroups.NotEmpty.class)
+    @Length(min = 2, max = 10, message = "아이디 길이는 최소 2자에서 최대 10자입니다.", groups = ValidationGroups.LimitCount.class)
     private String username;
-    @NotBlank(message = "비밀번호를 작성해주세요.")
-    @Length(min = 10, message = "비밀번호는 최소 10자입니다.")
+    @NotBlank(message = "비밀번호를 작성해주세요.", groups = ValidationGroups.NotEmpty.class)
+    @Length(min = 10, message = "비밀번호는 최소 10자입니다.", groups = ValidationGroups.LimitCount.class)
     private String password;
-    @NotNull(message = "닉네임을 작성해주세요.")
     private String nickname;
     private String address;
-    @NotBlank(message = "전화번호를 입력해주세요")
-    @Length(min = 11, max = 11)
+    @NotBlank(message = "전화번호를 입력해주세요", groups = ValidationGroups.NotEmpty.class)
     private String phone;
     private MultipartFile profileImg;
 
