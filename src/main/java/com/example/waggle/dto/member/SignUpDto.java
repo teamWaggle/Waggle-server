64f192e296d8b8e0d7c1a08cc6ee8b621d60ab48
suddiyo <suddii01@sju.ac.kr>
2023-09-24 14:30:34 +0900

    Update(BE): SignUpDto에서 profileImg 제거

--- src/main/java/com/example/waggle/dto/member/SignUpDto.java
+++ src/main/java/com/example/waggle/dto/member/SignUpDto.java
@@ -1,20 +1,13 @@
 package com.example.waggle.dto.member;
 
 import com.example.waggle.domain.member.Member;
-import com.example.waggle.domain.member.Pet;
-import com.example.waggle.domain.member.UploadFile;
 import com.example.waggle.dto.validation.ValidationGroups;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -22,7 +15,7 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @Builder
 @ToString
-public class SignUpDto {
+public class SignUpDto{
 
     @NotBlank(message = "아이디를 작성해주세요.", groups = ValidationGroups.NotEmpty.class)
     @Length(min = 2, max = 10, message = "아이디 길이는 최소 2자에서 최대 10자입니다.", groups = ValidationGroups.LimitCount.class)
@@ -34,7 +27,6 @@ public class SignUpDto {
     private String address;
     @NotBlank(message = "전화번호를 입력해주세요", groups = ValidationGroups.NotEmpty.class)
     private String phone;
-    private MultipartFile profileImg;
 
     @Builder.Default
     private List<PetDto> pets = new ArrayList<>();
