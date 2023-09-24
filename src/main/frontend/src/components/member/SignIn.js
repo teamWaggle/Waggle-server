0ee93f4920802a09773985234fad321ba976ed23
suddiyo <suddii01@sju.ac.kr>
2023-09-24 15:01:31 +0900

    Feat(FE): 로그인, 회원가입 api 설정

--- src/main/frontend/src/components/member/SignIn.js
+++ src/main/frontend/src/components/member/SignIn.js
@@ -5,17 +5,34 @@ import passwordIcon from '../../images/password.png';
 
 import styles from '../../styles/SignIn.module.css';
 
-function SignIn() {
+function SignIn({ updateFormData }) {
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        updateFormData(name, value);
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.containerItem}>
                 <div className={styles.inputContainer}>
                     <img src={userIcon} alt="userIcon" className={styles.inputItemImg}></img>
-                    <input placeholder="아이디" className={styles.inputItem}></input>
+                    <input 
+                    name="username"
+                    placeholder="아이디" 
+                    className={styles.inputItem}
+                    onChange={handleChange}
+                    ></input>
                 </div>
                 <div className={styles.lastInputContainer}>
                     <img src={passwordIcon} alt="passwordIcon" className={styles.inputItemImg}></img>
-                    <input placeholder="비밀번호" className={styles.inputItem}></input>
+                    <input 
+                    name="password"
+                    placeholder="비밀번호" 
+                    className={styles.inputItem} 
+                    type="password"
+                    onChange={handleChange}
+                    ></input>
                 </div>
             </div >
 
