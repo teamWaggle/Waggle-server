12d936356a01128008cdbd8124e3e6d40fcbf805
suddiyo <suddii01@sju.ac.kr>
2023-09-24 15:47:12 +0900

    Feat(FE): 로그아웃 api 설정

--- src/main/frontend/src/components/main/LoginedInfo.js
+++ src/main/frontend/src/components/main/LoginedInfo.js
@@ -2,7 +2,19 @@ import React from 'react';
 import defaultProfileImg from '../../images/defaultProfileImg.png';
 import styles from '../../styles/LoginedInfo.module.css';
 
+import { memberLogoutApi } from '../../apis/memberApi'
+
 function LoginedInfo({ loginedMember }) {
+    const handleLogout = () => {
+        memberLogoutApi()
+            .then(response => {
+                console.log('Logout successful:', response);
+                window.location.href = '/';
+            })
+            .catch(error => {
+                console.log('Logout failed:', error);
+            });
+    };
 
     return (
         <div className={styles.container}>
@@ -14,9 +26,10 @@ function LoginedInfo({ loginedMember }) {
                     <div className={styles.profileUsername}>{loginedMember.username}</div>
                     <div className={styles.profileNickname}>{loginedMember.nickname}</div>
                 </div>
-                <button onClick={() => window.location.href = '/member/sign-out'}
-                        className={styles.btnLogout}
-                        type="button">로그아웃
+                <button
+                    onClick={handleLogout}
+                    className={styles.btnLogout}
+                    type="button">로그아웃
                 </button>
             </div>
 
