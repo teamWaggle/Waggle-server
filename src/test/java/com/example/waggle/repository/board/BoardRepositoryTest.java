7197f7fc05aaf5cdb95ecba5097474e1be396d0c
suddiyo <suddii01@sju.ac.kr>
2023-09-27 18:59:46 +0900

    Update: 기존 template 및 일부 테스트 Disabled

--- src/test/java/com/example/waggle/repository/board/BoardRepositoryTest.java
+++ src/test/java/com/example/waggle/repository/board/BoardRepositoryTest.java
@@ -4,6 +4,7 @@ import com.example.waggle.domain.board.hashtag.BoardHashtag;
 import com.example.waggle.domain.board.hashtag.Hashtag;
 import com.example.waggle.domain.board.boardType.Story;
 import com.example.waggle.repository.board.boardtype.StoryRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +28,7 @@ class BoardRepositoryTest {
     @Test
     @Transactional
     @Rollback
+    @Disabled
     void test() {
 
         Story testStory = Story.builder().thumbnail("@hann").content("Board test Repository").build();
