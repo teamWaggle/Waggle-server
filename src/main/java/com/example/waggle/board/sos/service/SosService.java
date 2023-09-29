a82bc6a0be819ee13270eccb7c7d51266099cc75
suddiyo <suddii01@sju.ac.kr>
2023-09-29 23:08:52 +0900

    Refactor: service interface 생성

--- src/main/java/com/example/waggle/board/sos/service/SosService.java
+++ src/main/java/com/example/waggle/board/sos/service/SosService.java
@@ -1,4 +1,4 @@
 package com.example.waggle.board.sos.service;
 
-public class SosService {
+public interface SosService {
 }
--- src/main/java/com/example/waggle/pet/service/PetService.java
+++ src/main/java/com/example/waggle/pet/service/PetService.java
@@ -1,60 +1,16 @@
 package com.example.waggle.pet.service;
 
-import com.example.waggle.member.domain.Member;
-import com.example.waggle.pet.domain.Pet;
 import com.example.waggle.pet.dto.PetDto;
-import com.example.waggle.commons.exception.CustomAlertException;
-import com.example.waggle.commons.exception.CustomPageException;
-import com.example.waggle.member.repository.MemberRepository;
-import com.example.waggle.pet.repository.PetRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-import static com.example.waggle.commons.exception.ErrorCode.MEMBER_NOT_FOUND;
-import static com.example.waggle.commons.exception.ErrorCode.PET_NOT_FOUND;
+public interface PetService {
 
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-@Slf4j
-public class PetService {
-    private final MemberRepository memberRepository;
-    private final PetRepository petRepository;
+    public Optional<PetDto> findByPetId(Long petId);
 
-    public Optional<PetDto> findByPetId(Long petId) {
-        Optional<Pet> findPet = petRepository.findById(petId);
-        return findPet.map(PetDto::toDto);
-    }
+    public PetDto addPet(PetDto petDto);
 
-    @Transactional
-    public PetDto addPet(PetDto petDto) {
-        Member member = memberRepository.findByUsername(petDto.getUsername())
-                .orElseThrow(() -> new CustomPageException(MEMBER_NOT_FOUND));
-        Pet pet = petRepository.save(petDto.toEntity(member));
-        return PetDto.toDto(pet);
-    }
+    public PetDto updatePet(Long petId, PetDto petDto);
 
-    @Transactional
-    public PetDto updatePet(Long petId, PetDto petDto) {
-        Pet pet = petRepository.findById(petId)
-                .orElseThrow(() -> new CustomAlertException(PET_NOT_FOUND));
-        pet.update(petDto);
-        Pet updatedPet = petRepository.findById(petId).get();
-        return PetDto.toDto(updatedPet);
-    }
-
-    @Transactional
-    public Boolean removePet(Long petId) {
-        Optional<Pet> petToRemove = petRepository.findById(petId);
-        if(petToRemove.isPresent()) {
-            Pet pet = petToRemove.get();
-            petRepository.delete(pet);
-            return Boolean.TRUE;
-        }
-        return Boolean.FALSE;
-    }
+    public Boolean removePet(Long petId);
 }
