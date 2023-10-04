64086098978c6e85eecf80307277e3e20c2f27a4
hann <wjdgks1233@gmail.com>
2023-10-04 15:45:25 +0900

    FEAT: 최신순 sorting 추가(jpa query)

--- src/main/java/com/example/waggle/board/question/repository/QuestionRepository.java
+++ src/main/java/com/example/waggle/board/question/repository/QuestionRepository.java
@@ -1,6 +1,7 @@
 package com.example.waggle.board.question.repository;
 
 import com.example.waggle.board.question.domain.Question;
+import com.example.waggle.board.story.domain.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +9,6 @@ import java.util.List;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     List<Question> findByMemberUsername(String username);
+    List<Question> findAllByOrderByCreatedDateDesc();
 
 }
