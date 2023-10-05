dd184d9d943fb4c2785718ddeb9e64975f119d26
hann <wjdgks1233@gmail.com>
2023-10-05 13:31:28 +0900

    FEAT:pagination apply in question

--- src/main/java/com/example/waggle/board/question/repository/QuestionRepository.java
+++ src/main/java/com/example/waggle/board/question/repository/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.example.waggle.board.question.repository;
 
 import com.example.waggle.board.question.domain.Question;
 import com.example.waggle.board.story.domain.Story;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,5 +12,7 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     List<Question> findByMemberUsername(String username);
     List<Question> findAllByOrderByCreatedDateDesc();
+
+    Page<Question> findAll(Pageable pageable);
 
 }
