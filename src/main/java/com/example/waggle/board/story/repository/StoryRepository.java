48b702b0b33e231488d9ccd89272e857f02d3db3
hann <wjdgks1233@gmail.com>
2023-10-05 11:25:40 +0900

    FEAT: pagination spring tool apply and test

--- src/main/java/com/example/waggle/board/story/repository/StoryRepository.java
+++ src/main/java/com/example/waggle/board/story/repository/StoryRepository.java
@@ -1,6 +1,8 @@
 package com.example.waggle.board.story.repository;
 
 import com.example.waggle.board.story.domain.Story;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +13,8 @@ public interface StoryRepository extends JpaRepository<Story, Long> {
     List<Story> findByMemberUsername(String username);
 
     List<Story> findAllByOrderByCreatedDateDesc();      //최신순
+
+    Page<Story> findAll(Pageable pageable);
 
 
 }
