66d8bfc9bf558e0c94734fde02b70d999d1677c2
suddiyo <suddii01@sju.ac.kr>
2023-10-14 15:11:31 +0900

    Remove: remove frontend directory

--- src/main/frontend/src/components/ImageComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ImageComponent({ filename, className, onClick }) {
-  return (
-    <img src={`/images/${filename}`} alt="Description" className={className} onClick={onClick} />
-  );
-}
-
-export default ImageComponent;
