afe6fc36c065650f5afad722e035066e2af81cd1
Han-Jeong <69452755+Han-Jeong@users.noreply.github.com>
2023-12-05 19:31:18 +0900

    [Feat/#60] Implement Access Token Response and Refresh Token Storage Logic (#65)
    * [Feat/#60] Update SignIn Method to Return Access Token in Response
    
    * [Feat/#60] Implement Redis for refresh token storage
    
    * [Feat/#60] Implement token refresh logic
    
    * [Feat/#60] Implement logout logic
    
    * [Refactor/#60] Separate token-related logic into TokenController and TokenService
    
    ---------
    
    Co-authored-by: suddiyo <suddiyo@naver.com>

---
+++ src/main/java/com/example/waggle/domain/member/service/RedisService.java
@@ -0,0 +1,35 @@
+package com.example.waggle.domain.member.service;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final RedisTemplate redisTemplate;
+
+    // key-value 설정
+    public void setValue(String token, String username) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(token, username, Duration.ofDays(7));
+    }
+
+    // key 값으로 value 가져오기
+    public String getValue(String token) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        return values.get(token);
+    }
+
+    public void deleteValue(String token) {
+        if (token != null && token.startsWith("Bearer ")) {
+            // "Bearer " 접두사 제거
+            token = token.substring(7);
+        }
+        redisTemplate.delete(token);
+    }
+
+}
---
+++ src/main/java/com/example/waggle/global/security/TokenServiceImpl.java
@@ -0,0 +1,182 @@
+package com.example.waggle.global.security;
+
+import com.example.waggle.domain.member.service.MemberQueryService;
+import com.example.waggle.domain.member.service.RedisService;
+import com.example.waggle.web.dto.member.MemberRequest.LoginRequestDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class TokenServiceImpl implements TokenService {
+
+
+    private final Key key;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final RedisService redisService;
+    private final MemberQueryService memberQueryService;
+
+    public TokenServiceImpl(@Value("${jwt.secret}") String key,
+                            AuthenticationManagerBuilder authenticationManagerBuilder,
+                            RedisService redisService,
+                            MemberQueryService memberQueryService) {
+        byte[] keyBytes = Decoders.BASE64.decode(key);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.redisService = redisService;
+        this.memberQueryService = memberQueryService;
+    }
+
+    @Override
+    public JwtToken login(LoginRequestDto request) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                request.getUsername(), request.getPassword());
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        JwtToken jwtToken = generateToken(authentication);
+        redisService.setValue(jwtToken.getRefreshToken(), request.getUsername());
+        return jwtToken;
+    }
+
+    @Override
+    public JwtToken issueTokens(String refreshToken) {
+        // Refresh Token 유효성 검사
+        if (!validateToken(refreshToken) || !existsRefreshToken(refreshToken)) {
+            throw new RuntimeException("유효하지 않거나 만료된 리프레시 토큰입니다.");
+        }
+
+        // 이전 리프레시 토큰 삭제
+        redisService.deleteValue(refreshToken);
+
+        // 새로운 Authentication 객체 생성
+        Claims claims = parseClaims(refreshToken);
+        String username = claims.getSubject();
+        UserDetails userDetails = memberQueryService.getMemberByUsername(username);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, "",
+                userDetails.getAuthorities());
+
+        // 새 토큰 생성
+        JwtToken newTokens = generateToken(authentication);
+
+        return newTokens;
+    }
+
+    @Override
+    public JwtToken generateToken(Authentication authentication) {
+        // 권한 가져오기
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+
+        // Access Token 생성
+        Date accessTokenExpiresIn = new Date(now + 1800000);   // 30분
+        String accessToken = Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim("auth", authorities)
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        // Refresh Token 생성
+        String refreshToken = Jwts.builder()
+                .setSubject(authentication.getName())
+                .setExpiration(new Date(now + 604800000))    // 7일
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        // 새 리프레시 토큰을 Redis에 저장
+        redisService.setValue(refreshToken, authentication.getName());
+
+        return JwtToken.builder()
+                .grantType("Bearer")
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    @Override
+    public Authentication getAuthentication(String accessToken) {
+        // Jwt 토큰 복호화
+        Claims claims = parseClaims(accessToken);
+
+        if (claims.get("auth") == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        // 클레임에서 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get("auth").toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        // UserDetails 객체를 만들어서 Authentication return
+        // UserDetails: interface, User: UserDetails를 구현한 class
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+    @Override
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean logout(String refreshToken) {
+        redisService.deleteValue(refreshToken);
+        return true;
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(accessToken)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+    // RefreshToken 존재유무 확인
+    private boolean existsRefreshToken(String refreshToken) {
+        return redisService.getValue(refreshToken) != null;
+    }
+}
