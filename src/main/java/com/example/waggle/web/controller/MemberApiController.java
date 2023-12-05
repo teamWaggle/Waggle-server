ee085cc3c760255d8bf0faba55da395827768296
Han-Jeong <69452755+Han-Jeong@users.noreply.github.com>
2023-12-05 19:31:18 +0900

    [Feat/#60] Implement Access Token Response and Refresh Token Storage Logic (#65)
    * [Feat/#60] Update SignIn Method to Return Access Token in Response
    
    * [Feat/#60] Implement Redis for refresh token storage
    
    * [Feat/#60] Implement token refresh logic
    
    * [Feat/#60] Implement logout logic
    
    * [Refactor/#60] Separate token-related logic into TokenController and TokenService
    
    ---------
    
    Co-authored-by: suddiyo <suddiyo@naver.com>

--- src/main/java/com/example/waggle/web/controller/MemberApiController.java
+++ src/main/java/com/example/waggle/web/controller/MemberApiController.java
@@ -5,25 +5,17 @@ import com.example.waggle.domain.member.entity.Member;
 import com.example.waggle.domain.member.service.MemberCommandService;
 import com.example.waggle.domain.member.service.MemberQueryService;
 import com.example.waggle.global.payload.ApiResponseDto;
-import com.example.waggle.global.security.JwtToken;
 import com.example.waggle.web.converter.MemberConverter;
 import com.example.waggle.web.dto.member.MemberRequest;
 import com.example.waggle.web.dto.member.MemberResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseCookie;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,24 +32,6 @@ public class MemberApiController {
     private final MemberQueryService memberQueryService;
     private final AwsS3Service awsS3Service;
 
-    @Operation(summary = "로그인", description = "아이디와 비밀번호를 통해 로그인을 진행합니다. 성공 시 엑세스 토큰을 반환합니다.")
-    @ApiResponse(responseCode = "200", description = "로그인 성공. 엑세스 토큰을 반환합니다.")
-    @ApiResponse(responseCode = "401", description = "로그인 실패. 인증되지 않음.")
-    @PostMapping("/tokens")
-    public ApiResponseDto<String> login(@RequestBody MemberRequest.LoginRequestDto request,
-                                        HttpServletResponse response) {
-        JwtToken jwtToken = memberCommandService.signIn(request);
-
-        if (jwtToken != null) {
-            Cookie cookie = new Cookie("access_token", jwtToken.getAccessToken());
-            cookie.setPath("/");
-            cookie.setMaxAge(Integer.MAX_VALUE);
-            response.addCookie(cookie);
-            return ApiResponseDto.onSuccess(jwtToken.getAccessToken());
-        } else {
-            return ApiResponseDto.onFailure(401, "Unauthorized", null);
-        }
-    }
 
     @Operation(summary = "회원가입", description = "회원정보를 통해 회원가입을 진행합니다. 회원가입 후 회원 정보와 프로필 이미지를 반환합니다.")
     @ApiResponse(responseCode = "200", description = "회원가입 성공. 회원 정보 및 프로필 이미지를 반환합니다.")
@@ -67,26 +41,11 @@ public class MemberApiController {
             @RequestPart MemberRequest.RegisterRequestDto request,
             @RequestPart(value = "profileImg", required = false) MultipartFile profileImg) {
         String profileImgUrl = null;
-        if (profileImg != null) profileImgUrl = awsS3Service.uploadFile(profileImg);
+        if (profileImg != null) {
+            profileImgUrl = awsS3Service.uploadFile(profileImg);
+        }
         Member member = memberCommandService.signUp(request, profileImgUrl);
         return ApiResponseDto.onSuccess(MemberConverter.toMemberSummaryDto(member));
-    }
-
-    @Operation(summary = "로그아웃", description = "로그아웃을 진행합니다. 현재 사용자의 엑세스 토큰을 무효화하고 인증 정보를 제거합니다.")
-    @ApiResponse(responseCode = "200", description = "로그아웃 성공. 'success' 반환.")
-    @DeleteMapping("/tokens")
-    public ApiResponseDto<Boolean> logout(HttpServletRequest request, HttpServletResponse response) {
-        ResponseCookie deleteCookie = ResponseCookie.from("access_token", "")
-                .path("/")
-                .httpOnly(true)
-                .value(null)
-                .maxAge(0)
-                .build();
-        response.addHeader("Set-Cookie", deleteCookie.toString());
-
-        SecurityContextHolder.clearContext();
-
-        return ApiResponseDto.onSuccess(true);
     }
 
     @Operation(summary = "회원 정보 조회", description = "username을 통해 username, nickname, profileImg를 조회합니다.")
