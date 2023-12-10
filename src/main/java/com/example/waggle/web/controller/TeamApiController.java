5b31dd6e2054e850b8e818f75c513355a5c16dd3
suddiyo <suddiyo@naver.com>
2023-12-11 01:07:11 +0900

    [Feat/#73] 특정 사용자의 페이징된 팀 정보 조회 API 구현

--- src/main/java/com/example/waggle/web/controller/TeamApiController.java
+++ src/main/java/com/example/waggle/web/controller/TeamApiController.java
@@ -12,6 +12,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -38,15 +42,6 @@ public class TeamApiController {
     public ApiResponseDto<Long> createTeam(@RequestBody Post request) {
         Long createdTeamId = teamCommandService.createTeam(request);
         return ApiResponseDto.onSuccess(createdTeamId);
-    }
-
-    @Operation(summary = "팀 조회", description = "팀의 정보를 조회합니다.")
-    @ApiResponse(responseCode = "200", description = "팀 조회 성공.")
-    @ApiResponse(responseCode = "404", description = "팀을 찾을 수 없습니다.")
-    @GetMapping("/{teamId}")
-    public ApiResponseDto<TeamResponse.DetailDto> getTeam(@PathVariable Long teamId) {
-        Team team = teamQueryService.getTeamById(teamId);
-        return ApiResponseDto.onSuccess(TeamConverter.toDetailDto(team));
     }
 
     @Operation(summary = "팀 정보 업데이트", description = "팀의 정보를 업데이트합니다.")
@@ -112,5 +107,31 @@ public class TeamApiController {
         teamCommandService.respondToParticipation(teamId, username, accept);
         return ApiResponseDto.onSuccess(Boolean.TRUE);
     }
+
+    @Operation(summary = "팀 조회", description = "팀의 정보를 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "팀 조회 성공.")
+    @ApiResponse(responseCode = "404", description = "팀을 찾을 수 없습니다.")
+    @GetMapping("/{teamId}")
+    public ApiResponseDto<TeamResponse.DetailDto> getTeam(@PathVariable Long teamId) {
+        Team team = teamQueryService.getTeamById(teamId);
+        return ApiResponseDto.onSuccess(TeamConverter.toDetailDto(team));
+    }
+
+
+    @Operation(summary = "사용자 팀 조회", description = "해당 사용자가 속한 팀 정보를 페이징하여 제공합니다.")
+    @ApiResponse(responseCode = "200", description = "팀 조회 성공.")
+    @ApiResponse(responseCode = "404", description = "사용자 또는 팀을 찾을 수 없습니다.")
+    @GetMapping("/user/{username}/teams")
+    public ApiResponseDto<TeamResponse.SummaryListDto> getTeamsByUsername(
+            @PathVariable String username,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Team> teams = teamQueryService.getTeamsByUsername(username, pageable);
+        return ApiResponseDto.onSuccess(TeamConverter.toSummaryListDto(teams));
+    }
+
+
 
 }
