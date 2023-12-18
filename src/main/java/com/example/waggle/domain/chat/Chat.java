1507a9436ebe3d7246a4fcae60c967ed26bb2a6f
suddiyo <suddiyo@naver.com>
2023-12-19 03:02:36 +0900

    [Feat/#84] Chat 저장, 전달 도메인 작성

---
+++ src/main/java/com/example/waggle/domain/chat/Chat.java
@@ -0,0 +1,28 @@
+package com.example.waggle.domain.chat;
+
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "chat")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Chat {
+
+    @Id
+    private String id;
+    private Integer chatRoomId;
+    private Long senderId;
+    private String senderUsername;
+    private String contentType;
+    private String content;
+    private LocalDateTime sendDate;
+    private Integer readCount;
+
+}
---
+++ src/main/java/com/example/waggle/domain/chat/Message.java
@@ -0,0 +1,55 @@
+package com.example.waggle.domain.chat;
+
+import jakarta.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message implements Serializable {
+    private String id;
+    @NotNull
+    private Integer chatRoomId;
+    @NotNull
+    private String contentType;
+    @NotNull
+    private String content;
+    @NotNull
+    private Long senderId;
+    @NotNull
+    private String senderUsername;
+    private Long sendTime;
+    private Integer readCount;
+
+    public void setSendTimeAndSender(String senderUsername, Long senderId, LocalDateTime sendTime, Integer readCount) {
+        this.senderUsername = senderUsername;
+        this.senderId = senderId;
+        this.sendTime = sendTime.atZone(ZoneId.of("Asia/Seoul")).toInstant().toEpochMilli();
+        this.readCount = readCount;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Chat toEntity() {
+        return Chat.builder()
+                .senderUsername(senderUsername)
+                .senderId(senderId)
+                .chatRoomId(chatRoomId)
+                .contentType(contentType)
+                .content(content)
+                .sendDate(Instant.ofEpochMilli(sendTime).atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime())
+                .readCount(readCount)
+                .build();
+    }
+
+}
