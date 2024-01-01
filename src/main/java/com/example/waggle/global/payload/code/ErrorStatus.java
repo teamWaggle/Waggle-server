02c475d228d5f16f08fe755b36ea7a88e5988278
Han-Jeong <69452755+Han-Jeong@users.noreply.github.com>
2024-01-01 22:08:18 +0900

    [Feat/#82] OAuth2 login service(Google, Kakao, Naver) (#86)
    * [Refactor/#81] teamMember link method change & test code check
    
    * [Refactor/#81] entity field setting refactoring
    
    * [refactor/#81] AuthenticationPrincipal custom set feat
    
    * [Refactor/#81] followApiController ref
    
    * [Feat/#82] oauth2 setting
    
    * [Feat/#82] google,naver,kakao ID&PW was issued(application-security.yml)
    
    * [Feat/#82] resolve issue problem in Notion
    
    * [Feat/#82] naver, google login test

--- src/main/java/com/example/waggle/global/payload/code/ErrorStatus.java
+++ src/main/java/com/example/waggle/global/payload/code/ErrorStatus.java
@@ -25,6 +25,7 @@ public enum ErrorStatus implements BaseCode {
     AUTH_INVALID_TOKEN(UNAUTHORIZED, 4052, "유효하지 않은 인증 토큰입니다."),
     AUTH_UNAUTHORIZED_MEMBER(UNAUTHORIZED, 4053, "계정 정보를 찾을 수 없습니다."),
     AUTH_TOKEN_NO_AUTHORITY(UNAUTHORIZED, 4054, "권한 정보가 없는 토큰입니다."),
+    AUTH_REDIRECT_NOT_MATCHING(BAD_REQUEST,4055,"redirect uri가 서버 내 설정과 매칭되지 않습니다"),
 
 
     // 회원 관련 오류 (4100 ~ 4149)
