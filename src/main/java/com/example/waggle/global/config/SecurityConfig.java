82dc2379864a322aeea339db2bc04b3f58aa2ad6
Han-Jeong <69452755+Han-Jeong@users.noreply.github.com>
2024-01-01 22:08:18 +0900

    [Feat/#82] OAuth2 login service(Google, Kakao, Naver) (#86)
    * [Refactor/#81] teamMember link method change & test code check
    
    * [Refactor/#81] entity field setting refactoring
    
    * [refactor/#81] AuthenticationPrincipal custom set feat
    
    * [Refactor/#81] followApiController ref
    
    * [Feat/#82] oauth2 setting
    
    * [Feat/#82] google,naver,kakao ID&PW was issued(application-security.yml)
    
    * [Feat/#82] resolve issue problem in Notion
    
    * [Feat/#82] naver, google login test

--- src/main/java/com/example/waggle/global/config/SecurityConfig.java
+++ src/main/java/com/example/waggle/global/config/SecurityConfig.java
@@ -2,6 +2,10 @@ package com.example.waggle.global.config;
 
 import com.example.waggle.global.security.JwtAuthenticationFilter;
 import com.example.waggle.global.security.TokenService;
+import com.example.waggle.global.security.oauth2.CustomOAuth2UserService;
+import com.example.waggle.global.security.oauth2.cookie.CookieAuthorizationRequestRepository;
+import com.example.waggle.global.security.oauth2.handler.OAuth2AuthenticationFailureHandler;
+import com.example.waggle.global.security.oauth2.handler.OAuth2AuthenticationSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,30 +23,57 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final TokenService tokenService;
+    private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
+        httpSecurity
+                //cors(다른 자원 허용)
+                .cors().and()
                 // REST API이므로 basic auth 및 csrf 보안을 사용하지 않음
                 .httpBasic().disable()
                 .csrf().disable()
+                .formLogin().disable()
                 // JWT를 사용하기 때문에 세션을 사용하지 않음
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        httpSecurity
                 .authorizeHttpRequests()
                 // 해당 API에 대해서는 모든 요청을 허가
                 .requestMatchers("/**").permitAll()
+                .requestMatchers("/login/**").permitAll()
+                .requestMatchers("/oauth2/**").permitAll()
                 .requestMatchers("/images/**").permitAll()
 
-//                .requestMatchers("/", "/member/**", "/story", "/css/**", "/*.ico", "/error", "/images/**").permitAll() // 임시로 모든 API 허용
+                .requestMatchers("/", "/css/**", "/*.ico", "/error", "/images/**").permitAll() // 임시로 모든 API 허용
                 // USER 권한이 있어야 요청할 수 있음
 //                .requestMatchers("/member/test", "/story/write", "/story/edit").hasRole("USER")
                 // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
-//                .anyRequest().authenticated()
-                .and()
+                .anyRequest().authenticated();
+
                 // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
-                .addFilterBefore(new JwtAuthenticationFilter(tokenService), UsernamePasswordAuthenticationFilter.class)
-                .build();
+//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+
+        httpSecurity
+                .oauth2Login()
+                    .authorizationEndpoint().baseUri("/oauth2/authorize")  // 소셜 로그인 url
+                    .authorizationRequestRepository(cookieAuthorizationRequestRepository)  // 인증 요청을 cookie 에 저장
+                    .and()
+                    //userService()는 OAuth2 인증 과정에서 Authentication 생성에 필요한 OAuth2User 를 반환하는 클래스를 지정한다.
+                    .userInfoEndpoint().userService(customOAuth2UserService)  // 회원 정보 처리
+                    .and()
+                    .successHandler(oAuth2AuthenticationSuccessHandler)
+                    .failureHandler(oAuth2AuthenticationFailureHandler);
+        //order : jwtFilter -> usernamePasswordAuthentication
+        //username&password를 통한 검증 로직보다 jwt를 우선시하겠다는 의미
+//        httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return httpSecurity.build();
     }
 
     @Bean
@@ -50,6 +81,5 @@ public class SecurityConfig {
         // BCrypt Encoder 사용
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-
-
 }
+
