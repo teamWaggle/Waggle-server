fd50ddd31078764a533d644882475cecda942d60
hann <wjdgks1233@gmail.com>
2024-01-09 11:28:36 +0900

    [Refactor] NGINX set ref1

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,9 +8,8 @@ source ${ABSDIR}/profile.sh # 해당 코드로 profile.sh 내의 함수 사용
 REPOSITORY=/home/ec2-user/app/step3
 
 echo ">>> Build 파일 복사"
-echo ">>> cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo ">>> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)    # jar 이름 꺼내오기
@@ -27,6 +26,6 @@ IDLE_PROFILE=$(find_idle_profile)
 echo ">>> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-security.yml,/home/ec2-user/app/application-s3.yml,/home/ec2-user/app/application-real-db.yml,classpath:/application-real.yml, classpath:/application-real1.yml, classpath:/application-real2.yml \
+        -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-security.yml,/home/ec2-user/app/application-s3.yml,/home/ec2-user/app/application-real-db.yml,classpath:/application-real.yml,classpath:/application-$IDLE_PROFILE.yml \
         -Dspring.profiles.active=$IDLE_PROFILE \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
