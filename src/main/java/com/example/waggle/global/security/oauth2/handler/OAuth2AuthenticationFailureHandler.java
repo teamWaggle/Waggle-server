904789cc7f82af1b5eca9667e938dbdd7786ff7b
hann <wjdgks1233@gmail.com>
2024-01-13 14:24:33 +0900

    [Refactor/#95] oauth2 token serve logic ref

--- src/main/java/com/example/waggle/global/security/oauth2/handler/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/example/waggle/global/security/oauth2/handler/OAuth2AuthenticationFailureHandler.java
@@ -1,40 +1,39 @@
 package com.example.waggle.global.security.oauth2.handler;
 
-import com.example.waggle.global.security.oauth2.cookie.CookieAuthorizationRequestRepository;
 import com.example.waggle.global.security.oauth2.cookie.CookieUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
 
-import static com.example.waggle.global.security.oauth2.cookie.CookieAuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
 
 @Component
 @RequiredArgsConstructor
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
-    private final CookieAuthorizationRequestRepository authorizationRequestRepository;
+    @Value("${app.oauth2.authorizedRedirectUri}")
+    private String redirectUri;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request,
                                         HttpServletResponse response,
                                         AuthenticationException exception) throws ServletException, IOException {
-        String targetUrl = CookieUtil.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
-                .map(Cookie::getValue)
-                .orElse("/");
 
-        targetUrl = UriComponentsBuilder.fromUriString(targetUrl)
+
+        String targetUrl = UriComponentsBuilder.fromUriString(redirectUri)
                 .queryParam("error", exception.getLocalizedMessage())
                 .build().toUriString();
+        targetUrl = UriUtils.encode(targetUrl, "UTF-8");
 
-        authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 }
--- src/main/java/com/example/waggle/web/controller/TokenApiController.java
+++ src/main/java/com/example/waggle/web/controller/TokenApiController.java
@@ -1,14 +1,22 @@
 package com.example.waggle.web.controller;
 
+import com.example.waggle.global.exception.GeneralException;
 import com.example.waggle.global.payload.ApiResponseDto;
+import com.example.waggle.global.payload.code.ErrorStatus;
 import com.example.waggle.global.security.JwtToken;
 import com.example.waggle.global.security.TokenService;
+import com.example.waggle.global.security.oauth2.cookie.CookieUtil;
 import com.example.waggle.web.dto.member.MemberRequest;
+import com.example.waggle.web.dto.oauth.OAuthToken;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -27,6 +35,20 @@ public class TokenApiController {
     @PostMapping
     public ApiResponseDto<JwtToken> login(@RequestBody MemberRequest.LoginDto request) {
         JwtToken jwtToken = tokenService.login(request);
+        return ApiResponseDto.onSuccess(jwtToken);
+    }
+
+    @Operation(summary = "oauth2 로그인", description = "oauth2를 통해 리다이렉트 된 페이지 입니다. 정상적으로 호출 되었을 시 쿠키의 리프레시 토큰을 통해 jwt Token을 재발급합니다")
+    @ApiResponse(responseCode = "200", description = "로그인 성공. 토큰을 반환합니다")
+    @ApiResponse(responseCode = "401", description = "로그인 실패. 인증되지 않음.")
+    @GetMapping ("/oauth2")
+    public ApiResponseDto<JwtToken> loginByOAuth2(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = CookieUtil.getCookie(request, "refresh_token")
+                .map(cookie -> cookie.getValue())
+                .orElseThrow(() -> new GeneralException(ErrorStatus.AUTH_REFRESH_NOT_EXIST_IN_COOKIE));
+        CookieUtil.deleteCookie(request, response, "refresh_token");
+        JwtToken jwtToken = tokenService.issueTokens(refreshToken);
+        CookieUtil.addCookie(response,"refresh_token",jwtToken.getRefreshToken(),1800);
         return ApiResponseDto.onSuccess(jwtToken);
     }
 
