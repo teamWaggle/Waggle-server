c2798130644a8bbf59c59f19285b4b1f89226376
Han-Jeong <69452755+Han-Jeong@users.noreply.github.com>
2024-01-20 16:41:53 +0900

    [Refactor/#95] token relation logic ref (#97)
    * [Refactor/#95] oauth2 token serve logic ref
    
    * [Refactor/#95] tokenApiController redis check logic add
    
    * [Refactor/#95] tokenApiController ref(refresh in redis remove logic add and refresh in cookie use logic add
    
    * [Refactor/#95] spring-doc yml(sort setting) add

--- src/main/java/com/example/waggle/web/controller/TokenApiController.java
+++ src/main/java/com/example/waggle/web/controller/TokenApiController.java
@@ -1,12 +1,17 @@
 package com.example.waggle.web.controller;
 
+import com.example.waggle.global.exception.GeneralException;
 import com.example.waggle.global.payload.ApiResponseDto;
+import com.example.waggle.global.payload.code.ErrorStatus;
 import com.example.waggle.global.security.JwtToken;
 import com.example.waggle.global.security.TokenService;
+import com.example.waggle.global.util.CookieUtil;
 import com.example.waggle.web.dto.member.MemberRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -19,14 +24,27 @@ import org.springframework.web.bind.annotation.*;
 public class TokenApiController {
 
     private final TokenService tokenService;
+    private static int week = 604800;
 
 
     @Operation(summary = "로그인", description = "아이디와 비밀번호를 통해 로그인을 진행합니다. 성공 시 엑세스 토큰을 반환합니다.")
     @ApiResponse(responseCode = "200", description = "로그인 성공. 엑세스 토큰을 반환합니다.")
     @ApiResponse(responseCode = "401", description = "로그인 실패. 인증되지 않음.")
     @PostMapping
-    public ApiResponseDto<JwtToken> login(@RequestBody MemberRequest.LoginDto request) {
+    public ApiResponseDto<JwtToken> login(@RequestBody MemberRequest.LoginDto request, HttpServletResponse response) {
         JwtToken jwtToken = tokenService.login(request);
+        CookieUtil.addCookie(response, "refresh_token", jwtToken.getRefreshToken(), week);
+        return ApiResponseDto.onSuccess(jwtToken);
+    }
+
+    @Operation(summary = "oauth2 로그인", description = "oauth2를 통해 리다이렉트 된 페이지 입니다. 정상적으로 호출 되었을 시 쿠키의 리프레시 토큰을 통해 jwt Token을 재발급합니다")
+    @ApiResponse(responseCode = "200", description = "로그인 성공. 토큰을 반환합니다")
+    @ApiResponse(responseCode = "401", description = "로그인 실패. 인증되지 않음.")
+    @GetMapping("/oauth2")
+    public ApiResponseDto<JwtToken> loginByOAuth2(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = getRefreshInCookie(request, response);
+        JwtToken jwtToken = tokenService.issueTokens(refreshToken);
+        CookieUtil.addCookie(response, "refresh_token", jwtToken.getRefreshToken(), week);
         return ApiResponseDto.onSuccess(jwtToken);
     }
 
@@ -34,8 +52,10 @@ public class TokenApiController {
     @ApiResponse(responseCode = "200", description = "토큰 재발급 성공. 새로운 토큰들 반환.")
     @ApiResponse(responseCode = "401", description = "인증 실패. 리프레시 토큰이 유효하지 않음.")
     @PostMapping("/refresh")
-    public ApiResponseDto<JwtToken> refreshToken(@RequestHeader("X-Refresh-Token") String refreshToken) {
+    public ApiResponseDto<JwtToken> refreshToken(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = getRefreshInCookie(request, response);
         JwtToken newTokens = tokenService.issueTokens(refreshToken);
+
         return ApiResponseDto.onSuccess(newTokens);
     }
 
@@ -43,9 +63,26 @@ public class TokenApiController {
     @Operation(summary = "로그아웃", description = "로그아웃을 진행합니다. 현재 사용자의 엑세스 토큰을 무효화하고 인증 정보를 제거합니다.")
     @ApiResponse(responseCode = "200", description = "로그아웃 성공. 'success' 반환.")
     @DeleteMapping
-    public ApiResponseDto<Boolean> logout(@RequestHeader("X-Refresh-Token") String refreshToken) {
-        tokenService.logout(refreshToken);
+    public ApiResponseDto<Boolean> logout(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = getRefreshInCookie(request, response);
+        removeRefreshInRedis(request, response, refreshToken);
         return ApiResponseDto.onSuccess(true);
     }
 
+    private static String getRefreshInCookie(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = CookieUtil.getCookie(request, "refresh_token")
+                .map(cookie -> cookie.getValue())
+                .orElseThrow(() -> new GeneralException(ErrorStatus.AUTH_REFRESH_NOT_EXIST_IN_COOKIE));
+        CookieUtil.deleteCookie(request, response, refreshToken);
+        return refreshToken;
+    }
+
+    private void removeRefreshInRedis(HttpServletRequest request, HttpServletResponse response, String refreshToken) {
+        if (!tokenService.existsRefreshToken(refreshToken)) {
+            throw new GeneralException(ErrorStatus.AUTH_INVALID_REFRESH_TOKEN);
+        } else {
+            tokenService.logout(refreshToken);      //refresh delete in redis
+        }
+        CookieUtil.deleteCookie(request, response, "refresh_token");
+    }
 }
