6ad2b77cd58458040240c828f5e7a4a03cbc0af2
ahnsugyeong <ahnsugyeong@gmail.com>
2024-02-18 23:27:56 +0900

    [Feat/#125] Apply Enum Validation to SirenRequest

--- src/main/java/com/example/waggle/web/dto/siren/SirenRequest.java
+++ src/main/java/com/example/waggle/web/dto/siren/SirenRequest.java
@@ -3,6 +3,7 @@ package com.example.waggle.web.dto.siren;
 import com.example.waggle.domain.board.ResolutionStatus;
 import com.example.waggle.domain.board.siren.entity.SirenCategory;
 import com.example.waggle.domain.member.entity.Gender;
+import com.example.waggle.global.annotation.valid.ValidEnum;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,7 +28,11 @@ public class SirenRequest {
         private String lostLocate;
         private LocalDateTime lostDate;
         private String content;
-        private SirenCategory category;
-        private ResolutionStatus status;
+
+        @ValidEnum(target = SirenCategory.class)
+        private String category;
+
+        @ValidEnum(target = ResolutionStatus.class)
+        private String status;
     }
 }
