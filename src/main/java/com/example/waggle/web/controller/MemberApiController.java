e575cc62b497a60db970b1f999b3b6f4d7e85a1e
hann <wjdgks1233@gmail.com>
2024-03-03 17:10:40 +0900

    [HotFix] file -> memberProfileImg name convert

--- src/main/java/com/example/waggle/web/controller/MemberApiController.java
+++ src/main/java/com/example/waggle/web/controller/MemberApiController.java
@@ -21,23 +21,15 @@ import com.example.waggle.web.dto.member.VerifyMailRequest.EmailVerificationDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.time.LocalDate;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -70,7 +62,7 @@ public class MemberApiController {
     @PutMapping(value = "/info", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ApiResponseDto<Long> initializeMemberProfile(
             @RequestPart("memberProfileRequest") MemberProfileDto memberProfileRequest,
-            @RequestPart(value = "file", required = false) MultipartFile memberProfileImg,
+            @RequestPart(value = "memberProfileImg", required = false) MultipartFile memberProfileImg,
             @AuthUser Member member) {
         Long memberId = memberCommandService.initializeMemberProfile(memberProfileRequest, memberProfileImg, member);
         return ApiResponseDto.onSuccess(memberId);
