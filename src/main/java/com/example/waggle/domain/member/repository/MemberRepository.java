7664639381049df60b8824b019ccbc1251e9b8ff
hann <wjdgks1233@gmail.com>
2024-03-13 09:04:48 +0900

    [Refactor/#155] remove memberRepository method of findByEmailAndAuthProvider

--- src/main/java/com/example/waggle/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/waggle/domain/member/repository/MemberRepository.java
@@ -1,7 +1,6 @@
 package com.example.waggle.domain.member.repository;
 
 import com.example.waggle.domain.member.entity.Member;
-import com.example.waggle.global.security.oauth2.OAuth2UserInfoFactory.AuthProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -12,8 +11,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
 
     Optional<Member> findByEmail(String email);
-
-    Optional<Member> findByEmailAndAuthProvider(String email, AuthProvider authProvider);
 
     Optional<Member> findByNickname(String nickname);
 
