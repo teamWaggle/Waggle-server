c6bd2944a238b53f64b5cb0cf1284ce5a10b4301
hann <wjdgks1233@gmail.com>
2024-03-20 22:59:42 +0900

    resolve import error

--- src/main/java/com/example/waggle/web/controller/QuestionApiController.java
+++ src/main/java/com/example/waggle/web/controller/QuestionApiController.java
@@ -1,7 +1,5 @@
 package com.example.waggle.web.controller;
 
-import static com.example.waggle.web.dto.question.QuestionResponse.QuestionDetailDto;
-
 import com.example.waggle.domain.board.question.entity.Question;
 import com.example.waggle.domain.board.question.service.QuestionCommandService;
 import com.example.waggle.domain.board.question.service.QuestionQueryService;
@@ -21,8 +19,6 @@ import com.example.waggle.web.dto.question.QuestionResponse.RepresentativeQuesti
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -31,15 +27,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.example.waggle.web.dto.question.QuestionResponse.QuestionDetailDto;
 
 @Slf4j
 @RequiredArgsConstructor
