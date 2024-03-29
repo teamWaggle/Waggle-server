eb2f751b016d8b0fc7d673fcf92e75ee6298dd70
hann <wjdgks1233@gmail.com>
2024-03-29 15:39:40 +0900

    [Feat/#178] refactor recommend service constructure using redis

--- src/main/java/com/example/waggle/domain/recommend/repository/RecommendRepository.java
+++ src/main/java/com/example/waggle/domain/recommend/repository/RecommendRepository.java
@@ -3,10 +3,11 @@ package com.example.waggle.domain.recommend.repository;
 import com.example.waggle.domain.board.Board;
 import com.example.waggle.domain.member.entity.Member;
 import com.example.waggle.domain.recommend.entity.Recommend;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface RecommendRepository extends JpaRepository<Recommend, Long> {
 
@@ -18,6 +19,8 @@ public interface RecommendRepository extends JpaRepository<Recommend, Long> {
 
     List<Recommend> findByBoardId(Long boardId);
 
+    List<Recommend> findByMember(Member member);
+
     void deleteAllByBoardId(Long boardId);
 
     void deleteAllByMemberUsername(String username);
@@ -25,6 +28,8 @@ public interface RecommendRepository extends JpaRepository<Recommend, Long> {
     void deleteAllByMember(Member member);
 
     void deleteAllByBoard(Board board);
+
+    void deleteByBoardAndMember(Board board, Member member);
 
     @Query("SELECT r FROM Recommend r WHERE r.board.id IN (SELECT s.id FROM Siren s)")
     List<Recommend> findRecommendsForSirens();
