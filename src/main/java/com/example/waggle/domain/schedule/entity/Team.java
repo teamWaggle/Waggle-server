68a41ff6c40a7bdb421a1e80cfcc12df4082f182
hann <wjdgks1233@gmail.com>
2024-04-06 18:13:16 +0900

    [Refactor/#185] remove team Max size

--- src/main/java/com/example/waggle/domain/schedule/entity/Team.java
+++ src/main/java/com/example/waggle/domain/schedule/entity/Team.java
@@ -2,28 +2,13 @@ package com.example.waggle.domain.schedule.entity;
 
 import com.example.waggle.domain.member.entity.Member;
 import com.example.waggle.global.component.auditing.BaseEntity;
-import com.example.waggle.web.dto.oauth.OAuthToken.request;
 import com.example.waggle.web.dto.schedule.TeamRequest;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
 import java.util.ArrayList;
 import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
@@ -43,7 +28,6 @@ public class Team extends BaseEntity {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private TeamColor teamColor;
-    private Integer maxTeamSize;
 
 
     @Builder.Default
@@ -73,7 +57,6 @@ public class Team extends BaseEntity {
         this.description = updateTeamRequest.getDescription();
         this.coverImageUrl = updateTeamRequest.getCoverImageUrl();
         this.teamColor = TeamColor.valueOf(updateTeamRequest.getTeamColor());
-        this.maxTeamSize = updateTeamRequest.getMaxTeamSize();
     }
 
     public void updateLeader(Member teamLeader) {
--- src/main/java/com/example/waggle/web/dto/schedule/TeamRequest.java
+++ src/main/java/com/example/waggle/web/dto/schedule/TeamRequest.java
@@ -3,7 +3,6 @@ package com.example.waggle.web.dto.schedule;
 import com.example.waggle.domain.schedule.entity.TeamColor;
 import com.example.waggle.global.annotation.valid.enums.ValidEnum;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,8 +16,6 @@ import lombok.NoArgsConstructor;
 public class TeamRequest {
     private String name;
     private String description;
-    @Min(value = 1, message = "팀인원수는 최소 1명입니다.(팀 리더 포함 인원 수)")
-    private Integer maxTeamSize;
     private String coverImageUrl;
 
     @ValidEnum(target = TeamColor.class)
