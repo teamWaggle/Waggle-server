99f84136797b269b20139edf13fd4a92bec67928
ahnsugyeong <ahnsugyeong@gmail.com>
2024-04-14 00:41:38 +0900

    [HotFix/#198] Change findOverlappingSchedules query to use individual date and time parameters

--- src/main/java/com/example/waggle/domain/schedule/repository/MemberScheduleRepository.java
+++ src/main/java/com/example/waggle/domain/schedule/repository/MemberScheduleRepository.java
@@ -4,10 +4,10 @@ import com.example.waggle.domain.schedule.entity.MemberSchedule;
 import com.example.waggle.domain.schedule.entity.Schedule;
 import io.lettuce.core.dynamic.annotation.Param;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 public interface MemberScheduleRepository extends JpaRepository<MemberSchedule, Long> {
     List<MemberSchedule> findByMemberId(Long memberId);
@@ -35,10 +35,13 @@ public interface MemberScheduleRepository extends JpaRepository<MemberSchedule, 
     void deleteAllBySchedule(Schedule schedule);
 
 
-    @Query("SELECT ms.schedule FROM MemberSchedule ms WHERE ms.member.id = :memberId AND (" +
-            "(ms.schedule.startDate <= :schedule.endDate AND ms.schedule.endDate >= :schedule.startDate) AND " +
-            "(ms.schedule.startTime < :schedule.endTime AND ms.schedule.endTime > :schedule.startTime))")
+    @Query("SELECT ms.schedule FROM MemberSchedule ms WHERE ms.member.id = :memberId AND " +
+            "(ms.schedule.startDate <= :endDate AND ms.schedule.endDate >= :startDate) AND " +
+            "(ms.schedule.startTime < :endTime AND ms.schedule.endTime > :startTime)")
     List<Schedule> findOverlappingSchedules(@Param("memberId") Long memberId,
-                                            @Param("schedule") Schedule schedule);
+                                            @Param("startDate") LocalDate startDate,
+                                            @Param("endDate") LocalDate endDate,
+                                            @Param("startTime") LocalTime startTime,
+                                            @Param("endTime") LocalTime endTime);
 
 }
