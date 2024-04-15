f9fd47ca55d711a42194fc5c17a1656c8d672d2e
hann <wjdgks1233@gmail.com>
2024-04-15 22:50:51 +0900

    [Refactor/#209] add isGuest param when social login

--- src/main/java/com/example/waggle/global/security/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/example/waggle/global/security/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.example.waggle.global.security.oauth2.handler;
 
+import com.example.waggle.domain.member.entity.Role;
 import com.example.waggle.global.security.object.JwtToken;
 import com.example.waggle.global.security.service.TokenService;
 import com.example.waggle.global.util.CookieUtil;
@@ -9,12 +10,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.Collection;
 
 
 @Slf4j
@@ -36,15 +39,22 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
             return;
         }
         String provider = null;
+        boolean isGuest = false;
         if (authentication instanceof OAuth2AuthenticationToken) {
             OAuth2AuthenticationToken oauth2Token = (OAuth2AuthenticationToken) authentication;
             provider = oauth2Token.getAuthorizedClientRegistrationId();
+            Collection<GrantedAuthority> authorities = oauth2Token.getAuthorities();
+            isGuest = authorities.stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .anyMatch(Role.GUEST.getKey()::equals);
         }
+        log.info("redirectUri = {}", redirectUri);
         CookieUtil.addCookie(response, "refresh_token", jwtToken.getRefreshToken(), week);
         CookieUtil.deleteCookie(request, response, "JSESSIONID");
         String url = UriComponentsBuilder.fromHttpUrl(redirectUri)
                 .queryParam("code", jwtToken.getAccessToken())
                 .queryParam("provider", provider)
+                .queryParam("isGuest", isGuest)
                 .build()
                 .toUriString();
 
