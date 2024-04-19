786ef055c24c74f42c5ca55aad59f6cbc3f04232
Ahn Su-gyeong <ahnsugyeong@gmail.com>
2024-04-19 14:03:53 +0900

    [HotFix] Add Overlapped Schedule Logic to Paged API (#212)

--- src/main/java/com/example/waggle/domain/schedule/repository/TeamMemberRepository.java
+++ src/main/java/com/example/waggle/domain/schedule/repository/TeamMemberRepository.java
@@ -4,24 +4,12 @@ package com.example.waggle.domain.schedule.repository;
 import com.example.waggle.domain.member.entity.Member;
 import com.example.waggle.domain.schedule.entity.Team;
 import com.example.waggle.domain.schedule.entity.TeamMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
 
-    Optional<TeamMember> findTeamMemberByMemberUsernameAndTeamId(String username, Long teamId);
-
-    List<TeamMember> findTeamMemberByTeamId(Long teamId);
-
-    boolean existsByMemberUsername(String username);
-
     boolean existsByMemberAndTeam(Member member, Team team);
-
-    void deleteAllByMemberUsername(String username);
-
-    void deleteAllByMemberUsernameAndTeamId(String username, Long teamId);
 
     void deleteAllByMemberIdAndTeamId(Long memberId, Long teamId);
 
