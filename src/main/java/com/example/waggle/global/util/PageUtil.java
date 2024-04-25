c70659c7f2a9762e5fe021cae0ee62651aa89db3
hann <wjdgks1233@gmail.com>
2024-04-25 20:34:34 +0900

    [HotFix] convert constance to variable using pageutil

--- src/main/java/com/example/waggle/global/util/PageUtil.java
+++ src/main/java/com/example/waggle/global/util/PageUtil.java
@@ -7,6 +7,7 @@ public class PageUtil {
     public static int STORY_SIZE = 9;
     public static int QUESTION_SIZE = 9;
     public static int COMMENT_SIZE = 5;
+    public static int TEAM_RECOMMEND_SIZE = 3;
 
     public static int countNextPage(Page<?> pagedList) {
         if (pagedList.isLast()) return THIS_PAGE_IS_LAST;
