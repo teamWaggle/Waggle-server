b302df84650a32645dbf98633544ff14c62fead7
Ahn Su-gyeong <ahnsugyeong@gmail.com>
2024-04-26 03:49:58 +0900

    [Feat/#84] Implement Chatting Room Feature (#222)
    * [Feat/#84] Implement Chatting Room Feature
    
    * [Feat/#84] Update ChatRoom Feature

--- src/main/java/com/example/waggle/global/util/PageUtil.java
+++ src/main/java/com/example/waggle/global/util/PageUtil.java
@@ -7,10 +7,13 @@ public class PageUtil {
     public static int STORY_SIZE = 9;
     public static int QUESTION_SIZE = 9;
     public static int COMMENT_SIZE = 5;
+    public static int CHAT_ROOM_SIZE = 9;
     public static int TEAM_RECOMMEND_SIZE = 3;
 
     public static int countNextPage(Page<?> pagedList) {
-        if (pagedList.isLast()) return THIS_PAGE_IS_LAST;
+        if (pagedList.isLast()) {
+            return THIS_PAGE_IS_LAST;
+        }
         return pagedList.getNumber() + 1;
     }
 }
