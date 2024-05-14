4666fcd27bc25e27a95450022193d7072beadd7b
Ahn Su-gyeong <ahnsugyeong@gmail.com>
2024-05-14 17:14:19 +0900

    [HotFix] Disable withSockJS (#260)

--- src/main/java/com/example/waggle/global/config/websocket/WebSocketConfig.java
+++ src/main/java/com/example/waggle/global/config/websocket/WebSocketConfig.java
@@ -19,8 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
+                //.withSockJS();
     }
 
     @Override
