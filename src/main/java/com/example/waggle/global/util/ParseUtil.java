8caddb76c997b71d85ee5bb545c1778b54e1fbd0
hann <wjdgks1233@gmail.com>
2024-05-14 21:32:28 +0900

    [Feat/#256] refactor parseutil by feedback

--- src/main/java/com/example/waggle/global/util/ParseUtil.java
+++ src/main/java/com/example/waggle/global/util/ParseUtil.java
@@ -1,19 +1,26 @@
 package com.example.waggle.global.util;
 
 import com.example.waggle.domain.conversation.persistence.entity.Conversation;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Slf4j
 public class ParseUtil {
 
-    private static final String mentionRegex = "@\\[([^]]+)]\\(([^)]+)\\)";
+    private static final String MENTION_REGREX = "@\\[([^]]+)]\\(([^)]+)\\)";
+    private static final Pattern MENTION_PATTERN = Pattern.compile(MENTION_REGREX);
 
     public static List<String> parsingUserUrl(Conversation conversation) {
-        Pattern pattern = Pattern.compile(mentionRegex);
-        Matcher matcher = pattern.matcher(conversation.getContent());
+        if (conversation == null || conversation.getContent() == null) {
+            log.warn("NPE");
+            return new ArrayList<>();
+        }
+
+        Matcher matcher = MENTION_PATTERN.matcher(conversation.getContent());
         List<String> userUrlList = new ArrayList<>();
         while (matcher.find()) {
             userUrlList.add(matcher.group(2));
