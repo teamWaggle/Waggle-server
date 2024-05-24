153591db5f07f96aa6f81a947d4d200cbf6cc0f1
hann <wjdgks1233@gmail.com>
2024-05-24 11:06:03 +0900

    [Feat/#268] add query of delete team with relations

--- src/main/java/com/example/waggle/domain/schedule/persistence/dao/team/querydsl/TeamQueryRepository.java
+++ src/main/java/com/example/waggle/domain/schedule/persistence/dao/team/querydsl/TeamQueryRepository.java
@@ -2,4 +2,6 @@ package com.example.waggle.domain.schedule.persistence.dao.team.querydsl;
 
 public interface TeamQueryRepository {
     void deleteTeamWithRelationsByMemberId(Long memberId);
+
+    void deleteTeamWithRelations(Long teamId);
 }
