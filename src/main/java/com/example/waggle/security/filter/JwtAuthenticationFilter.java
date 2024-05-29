7424e8182d958ee2d5988e1db2abda3b32584d72
hann <wjdgks1233@gmail.com>
2024-05-29 23:57:29 +0900

    [Hotfix] remove log of health-check

--- src/main/java/com/example/waggle/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/waggle/security/filter/JwtAuthenticationFilter.java
@@ -39,7 +39,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             log.info("set Authentication to security context for '{}', uri: '{}', Role '{}'",
                     authentication.getName(), ((HttpServletRequest) request).getRequestURI(), authentication.getAuthorities());
         } else {
-            log.info("no valid JWT token found, uri: {}", ((HttpServletRequest) request).getRequestURI());
+            String uri = ((HttpServletRequest) request).getRequestURI();
+            if (!uri.equals("/health-check")) {
+                log.info("no valid JWT token found, uri: {}", uri);
+            }
         }
 
         chain.doFilter(request, response);
