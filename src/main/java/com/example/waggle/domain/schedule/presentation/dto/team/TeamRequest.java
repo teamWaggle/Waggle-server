2026617f0d63bfe6f6af4d3dea84663e0e705e89
hann <wjdgks1233@gmail.com>
2024-05-31 17:38:32 +0900

    [Refactor/#277] setting not null at teamRequest dto

--- src/main/java/com/example/waggle/domain/schedule/presentation/dto/team/TeamRequest.java
+++ src/main/java/com/example/waggle/domain/schedule/presentation/dto/team/TeamRequest.java
@@ -3,6 +3,7 @@ package com.example.waggle.domain.schedule.presentation.dto.team;
 import com.example.waggle.domain.schedule.persistence.entity.TeamColor;
 import com.example.waggle.global.annotation.valid.enums.ValidEnum;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +18,7 @@ public class TeamRequest {
     private String name;
     private String description;
     private String coverImageUrl;
+    @NotNull
     private Boolean isPrivate;
 
     @ValidEnum(target = TeamColor.class)
