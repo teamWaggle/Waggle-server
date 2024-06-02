6537e21ce047a299687aeafb1cdb62c9fbc360cf
hann <wjdgks1233@gmail.com>
2024-06-02 20:54:28 +0900

    [Refactor/#247] remove not necessary service of follow

--- src/main/java/com/example/waggle/domain/follow/application/FollowCommandService.java
+++ src/main/java/com/example/waggle/domain/follow/application/FollowCommandService.java
@@ -4,11 +4,7 @@ import com.example.waggle.domain.member.persistence.entity.Member;
 
 public interface FollowCommandService {
 
-    Long follow(String from, String to);
-
     Long follow(Member from, String to);
-
-    void unFollow(String from, String to);
 
     void unFollow(Member from, String to);
 
--- src/main/java/com/example/waggle/domain/follow/application/FollowQueryService.java
+++ src/main/java/com/example/waggle/domain/follow/application/FollowQueryService.java
@@ -6,15 +6,8 @@ import com.example.waggle.domain.member.persistence.entity.Member;
 import java.util.List;
 
 public interface FollowQueryService {
-    List<Follow> getFollowings(Long memberId);
-
-    List<Follow> getFollowingsByUsername(String username);
 
     List<Follow> getFollowingsByUserUrl(String userUrl);
-
-    List<Follow> getFollowers(Long memberId);
-
-    List<Follow> getFollowersByUsername(String username);
 
     List<Follow> getFollowersByUserUrl(String userUrl);
 
