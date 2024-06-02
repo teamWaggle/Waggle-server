f47f2dd4a6b835c9ca654366a18ae7e2f53968ed
hann <wjdgks1233@gmail.com>
2024-06-02 21:06:37 +0900

    [Refactor/#247] remove timeutil(no usages)

--- src/main/java/com/example/waggle/global/util/TimeUtil.java
+++
@@ -1,9 +0,0 @@
-package com.example.waggle.global.util;
-
-import java.time.LocalDateTime;
-
-public class TimeUtil {
-    public static LocalDateTime nowWithoutNano() {
-        return LocalDateTime.parse(LocalDateTime.now().toString().split("\\.")[0]);
-    }
-}
