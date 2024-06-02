986ce6ef6937e83c0a4afb894cefc61fbd618cfc
hann <wjdgks1233@gmail.com>
2024-06-02 20:55:20 +0900

    [Refactor/#247] remove not necessary service of hashtag

--- src/main/java/com/example/waggle/domain/hashtag/application/HashtagQueryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.waggle.domain.hashtag.application;
-
-import com.example.waggle.domain.hashtag.persistence.entity.Hashtag;
-
-import java.util.List;
-
-public interface HashtagQueryService {
-    List<Hashtag> getAllHashtags();
-}
