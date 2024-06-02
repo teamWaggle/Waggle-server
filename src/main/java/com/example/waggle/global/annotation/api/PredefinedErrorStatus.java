2c29bd431b55e089b466bcae7a040cc99ba99d4d
hann <wjdgks1233@gmail.com>
2024-06-03 06:26:47 +0900

    [Refactor/#247] refactor CommentApiController using predefinedErrorStatus

--- src/main/java/com/example/waggle/global/annotation/api/PredefinedErrorStatus.java
+++ src/main/java/com/example/waggle/global/annotation/api/PredefinedErrorStatus.java
@@ -9,20 +9,23 @@ public enum PredefinedErrorStatus {
     AUTH(List.of(
             ErrorStatus._INTERNAL_SERVER_ERROR,
             ErrorStatus._UNAUTHORIZED_LOGIN_DATA_RETRIEVAL_ERROR,
-            ErrorStatus._ASSIGNABLE_PARAMETER
+            ErrorStatus._ASSIGNABLE_PARAMETER,
+            ErrorStatus.MEMBER_NOT_FOUND
     )),
     BOARD_DATA_CHANGE(List.of(
             ErrorStatus._INTERNAL_SERVER_ERROR,
             ErrorStatus._UNAUTHORIZED_LOGIN_DATA_RETRIEVAL_ERROR,
             ErrorStatus._ASSIGNABLE_PARAMETER,
             ErrorStatus.BOARD_CANNOT_EDIT_OTHERS,
-            ErrorStatus.BOARD_NOT_FOUND
+            ErrorStatus.BOARD_NOT_FOUND,
+            ErrorStatus.MEMBER_NOT_FOUND
     )),
     ADMIN(List.of(
             ErrorStatus._INTERNAL_SERVER_ERROR,
             ErrorStatus._UNAUTHORIZED_LOGIN_DATA_RETRIEVAL_ERROR,
             ErrorStatus._ASSIGNABLE_PARAMETER,
-            ErrorStatus.AUTH_ROLE_CANNOT_EXECUTE_URI
+            ErrorStatus.AUTH_ROLE_CANNOT_EXECUTE_URI,
+            ErrorStatus.MEMBER_NOT_FOUND
     ));
 
     private final List<ErrorStatus> errorStatuses;
