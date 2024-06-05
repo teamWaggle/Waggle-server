a957ed899d5b9901affada0b2df685d29e45c04b
ahnsugyeong <ahnsugyeong@gmail.com>
2024-06-06 03:31:41 +0900

    [Feat/#300] Integrate chat room and chat messages deletion in deleteMember method

--- src/main/java/com/example/waggle/domain/chat/persistence/dao/ChatRoomRepository.java
+++ src/main/java/com/example/waggle/domain/chat/persistence/dao/ChatRoomRepository.java
@@ -3,10 +3,13 @@ package com.example.waggle.domain.chat.persistence.dao;
 import com.example.waggle.domain.chat.persistence.dao.querydsl.ChatRoomQueryRepository;
 import com.example.waggle.domain.chat.persistence.entity.ChatRoom;
 import com.example.waggle.domain.member.persistence.entity.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatRoomQueryRepository {
 
     void deleteAllByOwner(Member owner);
+
+    List<ChatRoom> findByOwner(Member owner);
 
 }
