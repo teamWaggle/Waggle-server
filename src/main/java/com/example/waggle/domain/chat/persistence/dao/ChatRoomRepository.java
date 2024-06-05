2bf463fabf7ac46587143847015f96157332891d
ahnsugyeong <ahnsugyeong@gmail.com>
2024-06-06 03:18:45 +0900

    [Feat/#300] Add deleteAllByOwner method to ChatRoomRepository

--- src/main/java/com/example/waggle/domain/chat/persistence/dao/ChatRoomRepository.java
+++ src/main/java/com/example/waggle/domain/chat/persistence/dao/ChatRoomRepository.java
@@ -2,7 +2,11 @@ package com.example.waggle.domain.chat.persistence.dao;
 
 import com.example.waggle.domain.chat.persistence.dao.querydsl.ChatRoomQueryRepository;
 import com.example.waggle.domain.chat.persistence.entity.ChatRoom;
+import com.example.waggle.domain.member.persistence.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatRoomQueryRepository {
+
+    void deleteAllByOwner(Member owner);
+
 }
