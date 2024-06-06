009489257635e983dc0aaef2bc08cc5af753f301
ahnsugyeong <ahnsugyeong@gmail.com>
2024-06-06 20:07:00 +0900

    [Feat/#300] Remove SockJS usage from chat template

--- src/main/java/com/example/waggle/global/config/websocket/WebSocketConfig.java
+++ src/main/java/com/example/waggle/global/config/websocket/WebSocketConfig.java
@@ -20,7 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
                 .setAllowedOriginPatterns("*");
-//                .withSockJS();
     }
 
     @Override
