a823408cb20c4c80c7eabb32446a522b31fd633b
hann <wjdgks1233@gmail.com>
2024-06-08 16:40:57 +0900

    [Refactor/#305] unify build method case

--- src/main/java/com/example/waggle/domain/notification/persistence/entity/Notification.java
+++ src/main/java/com/example/waggle/domain/notification/persistence/entity/Notification.java
@@ -1,17 +1,12 @@
 package com.example.waggle.domain.notification.persistence.entity;
 
 import com.example.waggle.domain.auditing.persistence.entity.BaseEntity;
-import com.example.waggle.domain.board.persistence.entity.BoardType;
-import com.example.waggle.domain.conversation.persistence.entity.Comment;
-import com.example.waggle.domain.follow.persistence.entity.Follow;
 import com.example.waggle.domain.member.persistence.entity.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-
-import static com.example.waggle.domain.notification.presentation.dto.NotificationRequest.ParticipationRequestDto;
 
 @Entity
 @Getter
@@ -42,40 +37,11 @@ public class Notification extends BaseEntity {
     public static Notification of(Member sender,
                                   Member receiver,
                                   NotificationType notificationType,
-                                  ParticipationRequestDto notificationRequest) {
+                                  String content) {
         return Notification.builder()
                 .sender(sender)
                 .receiver(receiver)
                 .type(notificationType)
-                .content(String.valueOf(notificationRequest))
-                .isRead(false)
-                .build();
-    }
-
-    public static Notification of(Member member, Follow follow) {
-        return Notification.builder()
-                .sender(member)
-                .type(NotificationType.FOLLOWED)
-                .receiver(follow.getToMember())
-                .isRead(false)
-                .build();
-    }
-
-    public static Notification of(Member member, Comment comment, BoardType boardType) {
-        return Notification.builder()
-                .sender(member)
-                .type(NotificationType.COMMENT)
-                .receiver(comment.getBoard().getMember())
-                .content(String.valueOf(boardType))
-                .isRead(false)
-                .build();
-    }
-
-    public static Notification of(Member sender, Member receiver, String content) {
-        return Notification.builder()
-                .sender(sender)
-                .type(NotificationType.MENTIONED)
-                .receiver(receiver)
                 .content(content)
                 .isRead(false)
                 .build();
