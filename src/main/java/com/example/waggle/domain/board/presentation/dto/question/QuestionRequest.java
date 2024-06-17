097691feb5123838e6d1b60bd368c94be7529221
ahnsugyeong <ahnsugyeong@gmail.com>
2024-06-17 17:41:33 +0900

    [Refactor/#294] Add validation for QuestionRequest

--- src/main/java/com/example/waggle/domain/board/presentation/dto/question/QuestionRequest.java
+++ src/main/java/com/example/waggle/domain/board/presentation/dto/question/QuestionRequest.java
@@ -1,16 +1,14 @@
 package com.example.waggle.domain.board.presentation.dto.question;
 
+import com.example.waggle.global.annotation.validation.hashtag.ValidHashtag;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
-
-import java.util.List;
 
 @Data
 @Builder
@@ -18,14 +16,19 @@ import java.util.List;
 @AllArgsConstructor
 @Schema
 public class QuestionRequest {
-    @NotEmpty(message = "질문 내용을 작성해주세요.")
-    @Size(max = 1500)
+
+    @NotEmpty(message = "내용을 작성해주세요.")
+    @Size(max = 1500, message = "내용은 최대 1500자까지 작성할 수 있습니다.")
     private String content;
 
-    @NotBlank(message = "질문 제목을 작성해주세요.")
-    @Length(min = 2, max = 30)
+    @Size(min = 2, max = 20, message = "제목을 2자 이상 20자 이하로 작성해주세요.")
     private String title;
 
+    @Size(max = 5, message = "해시태그는 최대 5개까지 작성할 수 있습니다.")
+    @ValidHashtag
     private List<String> hashtagList;
+
+    @Size(max = 10, message = "미디어는 최대 10개까지 추가할 수 있습니다.")
     private List<String> mediaList;
+
 }
